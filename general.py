114992dbbcb3cf342803d297f2469570575c39a7
25b1095 <prajwaljais72@gmail.com>
2026-04-08 20:39:31 +0530

    updated tic tac toe and general class

--- general.py
+++ general.py
@@ -1,18 +1,16 @@
-import pygame
 import numpy as np
-import sys #for standard input/output/exit,etc tasks like standard library of cpp
+import sys, os, time, pygame, pathlib
+from datetime import datetime
 
-class general :
-    ROWS=0
-    COLS=0
-    def __init__ (self,row,col,p1,p2,t):
-        self.ROWS=row
-        self.COLS=col
-        self.board = np.zeros((self.ROWS,self.COLS))
-        self.Player1=p1
-        self.Player2=p2
-        self.turn=t
-
-    def switch(self) :
-        self.turn=(self.turn%2)+1  
-
+pygame.init()
+class general:
+    def __init__(self, player1, player2):
+        self.players=["None",player1,player2]
+        self.current_turn = 1
+        self.area= None
+    def changeturn(self):
+        self.current_turn=self.current_turn%2 + 1
+    def currentturnplayer(self):
+        return self.players[self.current_turn]
+    def check_win(self):
+        raise NotImplementedError 
