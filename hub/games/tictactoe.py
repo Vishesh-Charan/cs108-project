c6d41d76bce5cc1d2a807be7585eb817e463704d
Vishesh-Charan <visheshcharan007@gmail.com>
2026-04-11 18:28:23 +0530

    Changed Tic Tac Toe, changed the board and background alongside some logic

--- hub/games/tictactoe.py
+++ hub/games/tictactoe.py
@@ -1,10 +1,11 @@
 import pygame as pg
 import numpy as np
 import sys
-from general import general
-
-
-
+import os
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+from game import general
 pg.init() 
 #initializing pygame
 
@@ -18,46 +19,36 @@ pg.display.set_caption("Tic Tac Toe")
 #now lets set a background color 
 bg_color=(28, 40, 51) #Dark Slate
 screen.fill(bg_color)
-side=60 
+sidex=48
+sidey=46
 # the side of each box = 600 pixels/10
 row,colomn=10,10
-player1=sys.argv[1]
-player2=sys.argv[2]
-
-
 class tic_tac_toe(general):
-    def __init__(self):
+    def __init__(self,player1,player2):
         super().__init__(player1,player2)
         self.board=np.zeros((row,colomn)) 
         #did same mistake the "tuple one"...
         #initialized the board
     def draw_lines(self):
-        #making the arena lines :)
-        #lets initialize the variables first 
-        color,border_size,side=(52, 73, 94),5,60
-        #its line's color,width and side lenth of square
-        # horizontal
-        for i in range(1, row):
-            pg.draw.line(screen,color, (0, i * side), (width, i * side), border_size)
-        # vertical
-        for i in range(1, colomn):
-            pg.draw.line(screen,color, (i * side, 0), (i * side, hieght), border_size)
-            
+        #Drawing the background board
+        BG_Image= pg.image.load('TTT Board.png')    
+        BG_Image=pg.transform.scale(BG_Image, (600,600))
+        screen.blit(BG_Image,(0,0))
+
     def draw_figures(self):
         #first lets define circle and cross color then its radius, dimensions , etc....
-
-        circle_color,cross_color=(241, 196, 15),(231, 76, 60) #Gold,Corral Red 
-        circle_width,cross_width,=8,10
-        radius,side=20,60
+        x=pg.image.load('X.png')
+        o=pg.image.load('O.png')
+        x=pg.transform.scale(x,(sidex-10,sidey-10))
+        o=pg.transform.scale(o,(sidex-10,sidey-10))
         for rows in range(row):
             for col in range(colomn):
                 if self.board[rows][col] == 1:
-                    #there is a inbuilt function to draw circle-
-                    pg.draw.circle(screen,circle_color, (int(col * side + side//2), int(rows * side + side//2)), radius, circle_width)
+                    #Insert O-
+                    screen.blit(o,(int(61+col * sidex + 5), int(76+rows * sidey + 5)))
                 elif self.board[rows][col] == 2:
-                    #cross using two diagonals
-                    pg.draw.line(screen, cross_color, (col * side + side//4, rows *side + (3*side)//4), (col * side + (3*side)//4, rows * side + side//4), cross_width)
-                    pg.draw.line(screen, cross_color, (col * side + side//4, rows * side + side//4), (col * side + (3*side)//4, rows * side + (3*side)//4), cross_width)
+                    #cross 
+                    screen.blit(x,(int(61+col * sidex + 5), int(76+rows * sidey + 5)))
 
     def mark_square(self,row, col, player):
         #to update that the box is now marked
@@ -65,7 +56,8 @@ class tic_tac_toe(general):
 
     def available_square(self,row, col):
         #to check status if the the box is empty or filled
-        return self.board[row][col] == 0      
+        if row<10 and row>=0 and col>=0 and col<10:
+            return self.board[row][col] == 0      
 
     def is_board_full(self):
         return not np.any(self.board == 0)
@@ -78,14 +70,27 @@ class tic_tac_toe(general):
         #set_of_subgrids is the variable representing all the possible 5*5 subsquares of the board
         anti_subgrids=np.flip(set_of_subgrids,axis=3)
         row_check=np.any(np.all(set_of_subgrids == player, axis=3))
+        if(row_check):
+            indices=np.argwhere(np.all(set_of_subgrids == player, axis=3))
+            return [True,indices[0],1]
         col_check=np.any(np.all(set_of_subgrids == player, axis=2))
+        if(col_check):
+            indices=np.argwhere(np.all(set_of_subgrids == player, axis=2))
+            return [True,indices[0],2]
         #axis represents the dimension we r considering in our 4D array
-
+        #in this I am returning a list True or False ndicate win or not. The indices array indicate the position variables needed to mark the line, like 
+        #the coordinates of the start of 5x5 subgrids and the row and col nos. in that subgrid by which I can make the lines later and also a no. from 1 to 4 indicating what kind
+        #of win it is, row,column,diagonal or  anti-diagonal
+         
         diag_check=np.any(np.all(np.diagonal(set_of_subgrids==player, axis1=2,axis2=3),axis=2))
+        if(diag_check):
+            indices=np.argwhere(np.all(np.diagonal(set_of_subgrids==player, axis1=2,axis2=3),axis=2))
+            return [True,indices[0],3]
         antidiag_check=np.any(np.all(np.diagonal(anti_subgrids==player,axis1=2,axis2=3),axis=2))
-        if(row_check or col_check or diag_check or antidiag_check):
-            return True
-        return False
+        if(antidiag_check):
+            indices=np.argwhere(np.all(np.diagonal(anti_subgrids==player, axis1=2,axis2=3),axis=2))
+            return [True,indices[0],4]
+        return [False,None,None]
         """
         #to check if the player wins
         #lined stack represents the number of X or O that should come in line to win
@@ -115,13 +120,11 @@ class tic_tac_toe(general):
     np.all is true if all conditions inside are true, np.any is true if atleast one condition is true
     the diagonal win check part was so much interesting and fun to code :)
     """
-    def restart(self):
-        screen.fill(bg_color)
-        self.draw_lines()
-        self.board[:][:]=0
-        #the restart function
     def run(self):
-
+        #background music
+        pg.mixer.init()
+        pg.mixer.music.load('8-Bit-Indigestion.mp3')
+        pg.mixer.music.play(-1)
         #initializing
         self.draw_lines()
         player = 1
@@ -143,16 +146,31 @@ class tic_tac_toe(general):
                     mouseX = event.pos[0]
                     mouseY = event.pos[1]
                     #storing the co ordinates of the pixel where the mouse got clicked
-                    clicked_row = int(mouseY // side)
-                    clicked_col = int(mouseX // side)
+                    clicked_row = int((mouseY-76) // sidey)
+                    clicked_col = int((mouseX-61) // sidex)
                     #found the corresponding row and colomn in 10*10 board
 
                     if self.available_square(clicked_row, clicked_col):
                         self.mark_square(clicked_row, clicked_col, player)
-                        if self.check_win(player):
+                        self.draw_figures()
+                        pg.display.update()
+                        pg.time.delay(500)
+                        stat=self.check_win(player)
+                        #Mark the tiles with a line to indicate Victory 
+                        if stat[0]:
+                            if stat[2]==1:
+                                pg.draw.line(screen,(128,128,128),(61+stat[1][1]*sidex+sidex//2,76+(stat[1][0]+stat[1][2])*sidey+sidey//2),(61+(stat[1][1]+4)*sidex+sidex//2,76+(stat[1][0]+stat[1][2])*sidey+sidey//2),width=3)
+                            elif stat[2]==2:
+                                pg.draw.line(screen,(128,128,128),(61+(stat[1][1]+stat[1][2])*sidex+sidex//2,76+stat[1][0]*sidey+sidey//2),(61+(stat[1][1]+stat[1][2])*sidex+sidex//2,76+(stat[1][0]+4)*sidey+sidey//2),width=3)
+                            elif stat[2]==3:
+                                pg.draw.line(screen,(128,128,128),(61+stat[1][0]*sidex+sidex//2,76+stat[1][1]*sidey+sidey//2),(61+(stat[1][0]+4)*sidex+sidex//2,76+(stat[1][1]+4)*sidey+sidey//2),width=3)
+                            elif stat[2]==4:
+                                pg.draw.line(screen,(128,128,128),(61+stat[1][0]*sidex+sidex//2,76+(stat[1][1]+4)*sidey+sidey//2),(61+(stat[1][0]+4)*sidex+sidex//2,76+stat[1][1]*sidey+sidey//2),width=3)
                             game_over = True
                             print(f"Player {player} wins!")
                             winner=self.currentturnplayer()
+                            pg.display.update()
+                            pg.time.delay(1500)
                             return winner
                         elif self.is_board_full():
                             game_over = True
@@ -162,21 +180,10 @@ class tic_tac_toe(general):
                         #player=player%2+1
                         self.changeturn()
                         player=self.current_turn
-                        self.draw_figures()
                         #this whole part handles the main gameplay using our functions, etc
-
-                if event.type == pg.KEYDOWN:
-                    if event.key == pg.K_r:
-                        #if user pressed 'r' on keyboard, we will restart 
-                        self.restart()
-                        player = 1
-                        game_over = False
-
             pg.display.update() 
             #as all happens in internal/memory/RAM whatever, we are updating it on display using this inbuilt function of pygame
             #tic tac toe finished finally ;)
 
 #i did this mistake - tic_tac_toe.run()
 #also remember to add self in inputs of functions and call functions as self.function
-game=tic_tac_toe()
-game.run()
